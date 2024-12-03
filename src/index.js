2b20710d7ea11aa88a586a4a46c409d015b5607a
anhtran <anhtr077@gmail.com>
2024-12-03 11:02:14 -0800

    update location and weather and connect with weather proxy server

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const landScape = document.getElementById("landscape");
 const cityNameInput = document.getElementById("cityNameInput");
 const cityNameReset = document.getElementById("cityNameReset");
 const headerCityName = document.getElementById("headerCityName");
+const currentTempButton = document.getElementById("currentTempButton");
+
 
 const updateTemperature = () => {
     tempValue.textContent = `${temperature}F`;
@@ -38,6 +40,42 @@ decreaseTempControl.addEventListener("click", () => {
     temperature -= 1;
     updateTemperature();
 });
+
+currentTempButton.addEventListener("click", () => {
+    const city = headerCityName.textContent;
+
+    axios.get('http://127.0.0.1:5000/location', {params:{"q": city}})
+    .then((locationResponse) => { 
+        const { lat, lon } = locationResponse.data;
+
+        return axios.get('http://127.0.0.1:5000/weather', {params:{"lat": lat, "lon": lon}})
+    })
+    
+});
+
+// @bp.get("/location")
+// def get_lat_lon():
+//     loc_query = request.args.get("q")
+//     if not loc_query:
+//         return {"message": "must provide q parameter (location)"}, 400
+
+//     response = requests.get(
+//         "https://us1.locationiq.com/v1/search.php",
+//         params={"q": loc_query, "key": location_key, "format": "json"}
+
+// def get_weather():
+//     lat_query = request.args.get("lat")
+//     lon_query = request.args.get("lon")
+
+//     if not lat_query or not lon_query:
+//         return {"message": "must provide lat and lon parameters"}, 400
+
+//     response = requests.get(
+//         "https://api.openweathermap.org/data/2.5/weather",
+//         params={"lat": lat_query, "lon": lon_query, "appid": weather_key}
+//     )
+//     return response.json(), 200
+
 
 updateTemperature();
 
