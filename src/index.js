b445734a0c4427d2a12119024b47b76c78bd20de
anhtran <anhtr077@gmail.com>
2024-12-05 19:10:14 -0800

    throw error to prevent executing undefined data

--- src/index.js
+++ src/index.js
@@ -106,6 +106,7 @@ const getCityLocation = (city) => {
     })   
     .catch((error) => {
         console.log('Error data:', error);
+        throw error
     });
 };
 
@@ -121,6 +122,7 @@ const getCityTemperature = (lat, lon) => {
     })
     .catch((error) => {
         console.log('Error data:', error);
+        throw error
     });
 };
 
